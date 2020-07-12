e705867d0372427a2ff3189603710291c04727d2
zhuzhaoman <2358793988@qq.com>
2020-07-12 18:47:36 +0800

    1、初始化项目 2、添加了Eureka

---
+++ README.md
@@ -0,0 +1,2 @@
+这是一个广告推送系统
+    采用SpringCloud+Mysql+Kafka设计
---
+++ advertisement-eureka/src/main/java/com/zzm/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.zzm.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zhuzhaoman
+ * @date 2020/7/12 0012 18:07
+ * @description 描述
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
